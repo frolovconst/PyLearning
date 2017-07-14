5a2171a5ed91c57442c8a95c3224a85c5ebd9c70
Konstantin Frolov <frolovconst@gmail.com>
2017-07-14 18:07:50 +0300

    ex2 started

--- ang/exercise/costFunctionJ.py
+++ ang/exercise/costFunctionJ.py
@@ -2,4 +2,4 @@ def costFunctionJ(X, y, theta):
         m = X.shape[0]
         predictions = X.dot(theta)
         sqrErrors = predictions - y
-        return sum(sqrErrors**2/2/m)
+        return sum(sqrErrors**2)/2/m
