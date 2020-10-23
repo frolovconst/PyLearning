d50b178388380cdee4226a8256167a57ebf4ba24
fkv <fkv@skbkontur.ru>
2020-10-23 11:32:36 +0500

    template edited

--- training/template.py
+++ training/template.py
@@ -3,13 +3,14 @@ from os.path import exists
 from pathlib import  Path
 FILE_NAME = 'stump.in'
 LOCAL_FOLDER_NAME = 'stump'
-LOCALDATA_ROOT_FOLDER_NAME = './data'
+LOCAL_DATA_ROOT_FOLDER_NAME = './data'
 
 
 def read_input(sort):
+    data_path = Path(LOCAL_DATA_ROOT_FOLDER_NAME) / LOCAL_FOLDER_NAME
     on_server = exists(FILE_NAME)
-    base_path = Path('.') if on_server else Path(DATA_ROOT_FOLDER_NAME) / LOCAL_FOLDER_NAME
-    with open(base_path + FILE_NAME, 'r') as f:
+    base_path = Path('.') if on_server else data_path
+    with open(base_path / FILE_NAME, 'r') as f:
         n = int(f.readline())
         if n == 0:
             result = None
